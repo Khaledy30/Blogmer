d45ee69e54b5d24fe9078403c04137c3207da89b
Khaledus <khaledyaurelio30@gmail.com>
2021-11-01 18:19:33 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+//articles.js route
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createAt: 'desc'})
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter) 
+
+app.listen(3333)
